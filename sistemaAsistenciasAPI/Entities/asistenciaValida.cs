a99bf75999fc2628f4e07da4a2b34b876ce680b7
Kevin Becerra <131114561+bcrraKevinho@users.noreply.github.com>
2023-05-24 03:20:25 -0700

    Add files via upload
    Fill folder

---
+++ sistemaAsistenciasAPI/Entities/asistenciaValida.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sistemaAsistenciasAPI.Entities
+{
+    public class asistenciaValida
+    {
+        public string mac { get; set; }
+        public string idsalon { get; set; }
+    }
+}
---
+++ sistemaAsistenciasAPI/Entities/consultaAsistencias.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sistemaAsistenciasAPI.Entities
+{
+    public class consultaAsistencias
+    {
+        public string idSalon { get; set; }
+        public DateTime horarioSeleccionado { get; set; }
+    }
+}
